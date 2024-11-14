2f85ac63a4cd3c38104d46c24e19244b42e97c1b
jinogr4 <jinog4@gmail.com>
2024-11-14 22:46:37 +0530

    Completed Framework

--- src/main/java/GR/Abstract/AbstractPage.java
+++ src/main/java/GR/Abstract/AbstractPage.java
@@ -3,11 +3,16 @@ package GR.Abstract;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.devtools.idealized.Javascript;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import GR.Pages.CartPage;
+import GR.Pages.OrdersPage;
 
 public class AbstractPage {
 
@@ -20,27 +25,51 @@ public class AbstractPage {
 	@FindBy(xpath = "//div[@class='ng-tns-c31-1 ng-star-inserted']")
 	WebElement LoadIcn;
 	
+	@FindBy(xpath = "//button[@routerlink='/dashboard/cart']")
+	WebElement CartBtn;
+	
+	@FindBy(xpath = "//button[@routerlink='/dashboard/myorders']") 
+	public WebElement OrdersButton;
+	
 	public static By LoadIcon = By.xpath("//div[@class='ng-tns-c31-1 ng-star-inserted']");
+	public static By CartButton = By.xpath("//button[@routerlink='/dashboard/cart']");
+
+	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 	
 	public void WaitForVisibilityOfElement(By Locator)
 	{
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 	    wait.until(ExpectedConditions.invisibilityOfElementLocated(Locator));
 
 	}
 	
 	public void WaitForInvisiblityOfElement(By Locator)
 	{
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		wait.until(ExpectedConditions.invisibilityOfElementLocated(Locator));
 
 	}
 	
 	public void WaitUntilElementToBeClicable(By Locator)
 	{
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 	    wait.until(ExpectedConditions.elementToBeClickable(Locator));
 
+	}
+	
+	public CartPage NavigateToCart() throws InterruptedException
+	{
+		
+		WaitUntilElementToBeClicable(CartButton);
+		Thread.sleep(2000);
+		CartBtn.click();
+		WaitUntilElementToBeClicable(CartPage.CheckOutBy);
+		CartPage CP = new CartPage(driver);
+		return CP;
+	}
+	
+	public OrdersPage GoToOrders()
+	{
+		OrdersButton.click();
+		OrdersPage OP = new OrdersPage(driver);
+		return OP;
 	}
 
 }
